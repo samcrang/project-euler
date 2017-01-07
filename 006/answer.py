d7aa321cdb4218bb32851186375868512eb8bc01
Sam Crang <sam.crang@gmail.com>
2017-01-07 21:24:43 +0000

    Add solution for 006

---
+++ 006/answer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+https://en.wikipedia.org/wiki/Square_pyramidal_number
+
+
+n(n + 1)(2n + 1) /6
+"""
+def sum_of_squares(n):
+    return n * (n + 1) * (2 * n + 1) / 6
+
+"""
+https://en.wikipedia.org/wiki/Triangular_number
+"""
+def sum(n):
+    return n * (n + 1) / 2
+
+def answer(n):
+    return int(sum(n)**2 - sum_of_squares(n))
+
+if __name__ == "__main__":
+    print(answer(100))
