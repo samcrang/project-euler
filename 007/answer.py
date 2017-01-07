028a6e134aca63b548f47dc0e3b0c637a13cb81c
Sam Crang <sam.crang@gmail.com>
2017-01-07 21:38:36 +0000

    Add solution for 007

---
+++ 007/answer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+"""
+def prime_sieve(size):
+    sieve = [True] * size
+    sieve[0] = False
+    sieve[1] = False
+
+    i = 2
+    while i < math.sqrt(size):
+        if sieve[i]:
+            j = int(math.pow(i, 2))
+            k = 0
+            while j < size:
+                sieve[j] = False
+                j = int(math.pow(i, 2)) + k * i
+                k += 1
+        i = i + 1
+
+    primes = []
+    for x in enumerate(sieve):
+        if x[1] == True:
+            primes.append(x[0])
+
+    return primes
+
+def answer():
+    primes = prime_sieve(900000)
+    return primes[10000]
+
+if __name__ == "__main__":
+    print(answer())
