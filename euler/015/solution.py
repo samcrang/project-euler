87dc113c07ce91becee6f691d9e970bbe2d56786
Sam Crang <sam.crang@gmail.com>
2017-02-19 23:02:56 +0000

    Add solution for 015

---
+++ euler/015/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+We can write a path through our lattice using "N" or "E". The number of Ns is the height of the lattice, the number of Es is the width of the lattice. Every path through the same lattice has exactly the same number of Es and Ns. We can just find the number of anagrams in order to find the number of paths.
+
+https://en.wikipedia.org/wiki/Lattice_path#North-East_lattice_paths
+https://en.wikipedia.org/wiki/Permutation#Permutations_of_multisets
+"""
+
+def answer(width, height):
+    return int(math.factorial(width + height)/(math.factorial(width) * math.factorial(height)))
+
+def solution():
+    return answer(20, 20)
+
+if __name__ == "__main__":
+    print(solution())
