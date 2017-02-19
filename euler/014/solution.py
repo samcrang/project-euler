8df59d28295dcad6f1db83a2fc4235e7cb275a49
Sam Crang <sam.crang@gmail.com>
2017-02-19 21:23:45 +0000

    Add unacceptably slow solution for 014
    ```
    $ time ./euler.py 014
    <solution>
    ./euler.py 014  64.04s user 0.15s system 99% cpu 1:04.43 total
    ```

---
+++ euler/014/solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def collatz(n):
+    sequence = [] 
+
+    while True:
+        sequence.append(int(n))
+
+        if n == 1:
+            return sequence
+
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+
+def answer(limit):
+    best_candidate = (0, 0)
+    for n in range(limit):
+        length = len(collatz(n + 1))
+        if length > best_candidate[1]:
+            best_candidate = (n + 1, length)
+    return best_candidate[0]
+
+def solution():
+    return answer(1000000)
+
+if __name__ == "__main__":
+    print(solution())
